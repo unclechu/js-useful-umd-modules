02c83764a9286c3315092935e9754d040b6997df
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-06-27 18:12:24 +0400

    "dragndrop_file_upload" module moved to own repository: https://github.com/unclechu/js-amd-dragndrop_file_upload

--- dragndrop_file_upload/src/Uploader/constructor/handlers/xhr_load.js
+++
@@ -1,9 +0,0 @@
-self._xhr.upload.addEventListener('load', function () { // {{{4
-	self.progress = 100;
-	self.finished = true;
-	if (self.superclass.params.progressCallback instanceof Function) {
-		self.superclass.params.progressCallback.call(
-			self, self.params.id, self.progress, self.loaded, self.total
-		);
-	}
-}); // 'load' event }}}4
