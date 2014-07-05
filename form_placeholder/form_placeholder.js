3779764383516db7157a0164a1167227909e4f96
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-07-06 01:34:25 +0400

    Update form_placeholder.js
    r3

--- form_placeholder/form_placeholder.js
+++ form_placeholder/form_placeholder.js
@@ -1,7 +1,7 @@
 /*!
  * Placeholder logic handler
  *
- * @version r2
+ * @version r3
  * @author Viacheslav Lotsmanov
  * @license GNU/GPLv3 by Free Software Foundation (https://github.com/unclechu/js-useful-amd-modules/blob/master/GPLv3-LICENSE)
  * @see {@link https://github.com/unclechu/js-useful-amd-modules/|GitHub}
@@ -16,10 +16,28 @@ define(['jquery', 'get_val'], function ($, getVal) {
 	 *
 	 * @this {DOM} - <label> that has <span> and <input> (or <textarea>)
 	 */
-	return function () {
+	return function (params) {
+
+		params = $.extend({
+			$placeholder: null,
+			placeholderSelector: '>span, .placeholder',
+			$input: null,
+			inputSelector: 'input, textarea'
+		}, (($.isPlainObject(params)) ? params : {}));
 
 		var $label = $(this);
-		var $placeholder = $label.find('span');
+		var $placeholder;
+		var $input;
+
+		if (params.$placeholder)
+			$placeholder = $( params.$placeholder );
+		else
+			$placeholder = $label.find( params.placeholderSelector );
+
+		if (params.$input)
+			$input = $( params.$input );
+		else
+			$input = $label.find( params.inputSelector );
 
 		function blurHandler() {
 			if ($(this).val() === '') {
@@ -33,7 +51,7 @@ define(['jquery', 'get_val'], function ($, getVal) {
 			$placeholder.stop().fadeOut(getVal('animationSpeed'));
 		}
 
-		$label.find('input, textarea')
+		$input
 			.on('focus' + bindSuffix, focusHandler)
 			.on('blur' + bindSuffix, blurHandler)
 			.trigger('blur' + bindSuffix)
